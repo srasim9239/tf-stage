0c00bf57cffc5bf1a2a79dbb360732539004b8c0
Rasim Shaykhullin <srasim9239@gmail.com>
2022-07-20 13:30:01 +0300

    tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "yandex-token" {
+  description = "YC OAuth token"
+  default     = ""
+
+}
+
+variable "yandex-cloud-id" {
+  description = "ID of a cloud"
+
+  default     = ""
+}
+
+variable "yandex-folder-id" {
+  description = "ID of a folder"
+
+}
